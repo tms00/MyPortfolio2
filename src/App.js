162c892fa5648458b3c409b3af89bb6195142df4
tms00 <32330114+tms00@users.noreply.github.com>
2020-02-10 12:38:18 -0300

    A ffew changes
    added a navbar

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 import './App.css';
 import './global.css';
 import './frontpage.css';
+import './navbar.css';
+import gitLogo from './assets/GitHub-Mark-64px.png'
 
 function App() {
   return (
-    <div className = "content">
+    <>
+    <head>
 
-      <div className = "frontPage">
+    </head>
 
-        <h1><strong>Hello, my name is Thiago.<br/> 
-        Student, and a full-stack developer.</strong></h1>
+    <body>
+      <div className = "content">
+        <div className = "navbar">
+            <ul>
+              <li><button className = "btn">Github</button></li>
+              <li><button className = "btn">About me</button></li>
+              <li><button className = "btn">Portfolio</button></li>
+            </ul>
+        </div>
+
+        <div className = "frontPage">
+          <h1><strong>Hello, my name is Thiago.<br/> 
+          Student, and a full-stack developer.</strong></h1>
+          <hr/>
+        </div>
+
+        <div className = "sideBar">
+          
+        </div>
 
       </div>
+    </body>
 
-
-    </div>
+    </>
   );
 }
 
